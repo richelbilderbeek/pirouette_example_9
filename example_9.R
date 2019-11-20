c96e86fe98effa1087b6edd050b4b21d9db03da1
richelbilderbeek <richel@richelbilderbeek.nl>
2019-11-20 09:43:32 +0100

    Correct filename in MCMCs, go AppVeyor

--- example_9.R
+++ example_9.R
@@ -29,11 +29,21 @@ alignment_params <- create_alignment_params(
     mutation_rate = 0.1
   ),
   root_sequence = create_blocked_dna(length = 1000),
-  rng_seed = rng_seed
+  rng_seed = rng_seed,
+  fasta_filename = "true_alignment.fas"
 )
 
-experiment <- create_gen_experiment()
-experiments <- list(experiment)
+# JC69, strict, Yule
+generative_experiment <- create_gen_experiment()
+generative_experiment$beast2_options$input_filename <- "true_alignment_gen.xml"
+generative_experiment$beast2_options$output_state_filename <- "true_alignment_gen.xml.state"
+generative_experiment$inference_model$mcmc$tracelog$filename <- "true_alignment_gen.log"
+generative_experiment$inference_model$mcmc$treelog$filename <- "true_alignment_gen.trees"
+generative_experiment$inference_model$mcmc$screenlog$filename <- "true_alignment_gen.csv"
+generative_experiment$errors_filename <- "true_errors_gen.csv"
+check_experiment(generative_experiment)
+
+experiments <- list(generative_experiment)
 
 # Set the RNG seed
 for (i in seq_along(experiments)) {
@@ -47,15 +57,23 @@ if (1 == 2) {
   }
 }
 
+twinning_params <- create_twinning_params(
+  rng_seed_twin_tree = rng_seed,
+  sim_twin_tree_fun = create_sim_bd_twin_tree_fun(),
+  rng_seed_twin_alignment = rng_seed,
+  sim_twin_alignment_fun = get_sim_twin_alignment_with_same_n_mutation_fun(
+    mutation_rate = 0.1,
+    max_n_tries = 1000
+  ),
+  twin_tree_filename = "twin_tree.newick",
+  twin_alignment_filename = "twin_alignment.fasta",
+  twin_evidence_filename = "twin_evidence.csv"
+)
+
 pir_params <- create_pir_params(
   alignment_params = alignment_params,
   experiments = experiments,
-  twinning_params = create_twinning_params(
-    rng_seed_twin_tree = rng_seed,
-    rng_seed_twin_alignment = rng_seed,
-    twin_tree_filename = "tree_twin.newick",
-    twin_alignment_filename = "alignment_twin.fasta"
-  )
+  twinning_params = twinning_params
 )
 
 rm_pir_param_files(pir_params)
