296c464b3dfe3978ad3014633e006aa57ccb5c1a
richelbilderbeek <richel@richelbilderbeek.nl>
2020-01-15 11:46:31 +0100

    EOD

--- example_9.R
+++ example_9.R
@@ -46,9 +46,6 @@ if (is_on_travis()) {
   for (i in seq_along(experiments)) {
     experiments[[i]]$inference_model$mcmc$chain_length <- 3000
     experiments[[i]]$inference_model$mcmc$store_every <- 1000
-    #experiments[[i]]$est_evidence_mcmc$chain_length <- 3000
-    #experiments[[i]]$est_evidence_mcmc$store_every <- 1000
-    #experiments[[i]]$est_evidence_mcmc$epsilon <- 100.0
   }
 }
 
