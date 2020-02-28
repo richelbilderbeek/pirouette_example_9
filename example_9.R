ee77e76780e473971df1ffa01ebd4c5bc039a815
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-28 13:10:01 +0100

    EOD

--- example_9.R
+++ example_9.R
@@ -1,3 +1,6 @@
+#
+# No candidates
+#
 library(pirouette)
 suppressMessages(library(ggplot2))
 library(beautier)
@@ -6,44 +9,19 @@ library(beautier)
 # Constants
 ################################################################################
 is_testing <- is_on_travis()
-
-root_folder <- getwd()
 example_no <- 9
 rng_seed <- 314
-example_folder <- file.path(root_folder, paste0("example_", example_no, "_", rng_seed))
-dir.create(example_folder, showWarnings = FALSE, recursive = TRUE)
-setwd(example_folder)
-set.seed(rng_seed)
-testit::assert(is_beast2_installed())
+folder_name <- paste0("example_", example_no, "_", rng_seed)
+
 phylogeny  <- ape::read.tree(
   text = "(((A:8, B:8):1, C:9):1, ((D:8, E:8):1, F:9):1);"
 )
-ape::write.tree(phylogeny, file = "tree_true.fas")
 
-alignment_params <- create_alignment_params(
-  sim_tral_fun = get_sim_tral_with_std_nsm_fun(
-    mutation_rate = 0.1
-  ),
-  root_sequence = create_blocked_dna(length = 1000)
+pir_params <- create_std_pir_params(
+  folder_name = folder_name
 )
-
-# JC69, strict, Yule
-generative_experiment <- create_gen_experiment()
-experiments <- list(generative_experiment)
-
-twinning_params <- create_twinning_params(
-  sim_twin_tree_fun = get_sim_bd_twin_tree_fun(),
-  sim_twal_fun = get_sim_twal_same_n_muts_fun(
-    mutation_rate = 0.1,
-    max_n_tries = 1000
-  )
-)
-
-pir_params <- create_pir_params(
-  alignment_params = alignment_params,
-  experiments = experiments,
-  twinning_params = twinning_params
-)
+# Remove candidates
+pir_params$experiments <- pir_params$experiments[1]
 
 # Shorter on Travis
 if (is_testing) {
@@ -55,22 +33,24 @@ errors <- pir_run(
   pir_params = pir_params
 )
 
+ape::write.tree(phylogeny, file = "tree_true.fas")
+
 utils::write.csv(
   x = errors,
-  file = file.path(example_folder, "errors.csv"),
+  file = file.path(folder_name, "errors.csv"),
   row.names = FALSE
 )
 
 pir_plot(errors) +
-  ggsave(file.path(example_folder, "errors.png"))
+  ggsave(file.path(folder_name, "errors.png"))
 
 pir_to_pics(
   phylogeny = phylogeny,
   pir_params = pir_params,
-  folder = example_folder
+  folder = folder_name
 )
 
 pir_to_tables(
   pir_params = pir_params,
-  folder = example_folder
+  folder = folder_name
 )
