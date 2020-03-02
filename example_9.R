cf7f25f9f8cea53c9d9b6ec47522352d299ec739
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-02 13:48:00 +0100

    Use of 'pir_save'

--- example_9.R
+++ example_9.R
@@ -1,48 +1,41 @@
+# pirouette example 9
 #
-# No candidates
+# * phylogeny: fictional
+# * pirouette setup: no candidats
+#
+# Other examples can be found 
+# at https://github.com/richelbilderbeek/pirouette_examples 
 #
 library(pirouette)
 library(beautier)
 
-################################################################################
 # Constants
-################################################################################
 is_testing <- is_on_travis()
 example_no <- 9
 rng_seed <- 314
 folder_name <- paste0("example_", example_no, "_", rng_seed)
 
-################################################################################
 # Create phylogeny
-################################################################################
 phylogeny  <- ape::read.tree(
   text = "(((A:8, B:8):1, C:9):1, ((D:8, E:8):1, F:9):1);"
 )
-
-################################################################################
 # Setup pirouette
-################################################################################
 pir_params <- create_std_pir_params(
   folder_name = folder_name
 )
 # Remove candidates
 pir_params$experiments <- pir_params$experiments[1]
-
+pir_params$evidence_filename <- NA
+pir_params$twinning_params$twin_evidence_filename <- NA
 if (is_testing) {
   pir_params <- shorten_pir_params(pir_params)
 }
-
-################################################################################
 # Run pirouette
-################################################################################
 pir_out <- pir_run(
   phylogeny,
   pir_params = pir_params
 )
-
-################################################################################
 # Save results
-################################################################################
 pir_save(
   phylogeny = phylogeny,
   pir_params = pir_params,
